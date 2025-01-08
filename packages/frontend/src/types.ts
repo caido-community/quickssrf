06e3683bb5debae6082056b3ea371f513194e6b3
corb3nik <ian@atvh.dev>
2025-01-08 02:06:19 -0500

    Move App.vue logic to own file

--- packages/frontend/src/types.ts
+++ packages/frontend/src/types.ts
@@ -3,13 +3,13 @@ import type { API } from "backend";
 
 export type FrontendSDK = Caido<API, never>;
 
-export interface Response {
-    protocol: string;
-    uniqueId: string; // Converted to camelCase for consistency
-    fullId: string; // Converted to camelCase for consistency
-    qType: string; // Query type
-    rawRequest: string;
-    rawResponse: string;
-    remoteAddress: string;
-    timestamp: string; // Use Date type for parsing if necessary
-  }
+export type Response = {
+  protocol: string;
+  uniqueId: string; // Converted to camelCase for consistency
+  fullId: string; // Converted to camelCase for consistency
+  qType: string; // Query type
+  rawRequest: string;
+  rawResponse: string;
+  remoteAddress: string;
+  timestamp: string; // Use Date type for parsing if necessary
+};
