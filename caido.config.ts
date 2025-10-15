5f7d353f8f31cf0e07a699648663ad8b4c4e35aa
Maxime <maxime.paille@mcn.gouv.qc.ca>
2025-10-15 18:09:43 -0400

    new version

--- caido.config.ts
+++ caido.config.ts
@@ -11,7 +11,7 @@ const id = "quickssrf";
 export default defineConfig({
   id,
   name: "QuickSSRF",
-  version: "0.1.9",
+  version: "0.2.0",
   description: "Real-time Interaction Monitoring with Interactsh",
   author: {
     name: "w2xim3",
